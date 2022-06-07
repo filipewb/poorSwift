758020469b2c141b1390eeb5250ea46ec3763a52
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2022-06-07 19:35:57 -0300

    first commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Filipe Wolf on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
